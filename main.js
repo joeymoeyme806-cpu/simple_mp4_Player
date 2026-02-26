89d22e12081e1c6cc1be9c58b9be7450c9f567e6
joeymoeyme806-cpu <joeymoeyme806@gmail.com>
2026-02-26 10:04:56 -0700

    Add volume control and file input for media player

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const player = document.getElementById('player');  
 const speedSlider = document.getElementById('speedSlider');  
 const speedValue = document.getElementById('speedValue');  
+const volumeSlider = document.getElementById('volumeSlider');  
+const fileInput = document.getElementById('fileInput');  
   
 function skip(seconds) {  
   player.currentTime += seconds;  
@@ -9,4 +11,18 @@ function skip(seconds) {
 speedSlider.oninput = function() {  
   player.playbackRate = this.value;  
   speedValue.textContent = this.value + "x";  
+};  
+  
+volumeSlider.oninput = function() {  
+  player.volume = this.value;  
+};  
+  
+fileInput.onchange = function() {  
+  const file = this.files[0];  
+  if (file) {  
+    const url = URL.createObjectURL(file);  
+    player.src = url;  
+    player.load();  
+    player.play();  
+  }  
 };  
